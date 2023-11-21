f8b5328c8dc4ba904988ecc7b49e60e92de45026
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2023-11-21 14:24:17 +0800

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -66,7 +66,7 @@ def run_ml_app():
     service = st.number_input("Length of Service",1,37)
     awards = st.radio("Awards Won", [0,1])
     avg_training = st.number_input("Average Training Score",0,100)
-    picklong = st.number_input(“Pickup Location Longitude”, 73, 75)
+    picklong = st.number_input(“Pickup Location Longitude”,73,75)
     
     with st.expander("Your Selected Options"):
         result = {
