a116ce5359bf48ae4364d7a6082eba96193cf356
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2023-11-21 18:40:32 +0800

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -113,6 +113,18 @@ def run_ml_app():
             'avg_training_score':avg_training,
         }
 
+    ml_data = {"Pickup longitude" : [picklong],
+                "Pickup latitude" : [picklat],
+                "Dropoff longitude" : [droplong],
+                "Dropoff latitude" : [droplat],
+                "Passenger count" : [passcount],
+                "year" : [pickup_dt.year],
+                "month" : [pickup_dt.month],
+                "day" : [pickup_dt.day],
+                "weekday" : [pickup_dt.weekday()],
+                "hour" : [pickup_tm.hour],
+                "Distance_in_Km" : [distance]}
+        
     df_new = pd.DataFrame(ml_data)
     model_reg, scaler = joblib.load('model_with_scaler.joblib')
     scaled_data = scaler.transform(df_new)
