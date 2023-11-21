425372546e83f5604835b65ea3aa69f97638cf46
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2023-11-21 22:35:21 +0800

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -51,8 +51,7 @@ def run_ml_app():
                 "Dropoff Location" : [(droplat, droplong)],
                 "Number of Passenger" : [passcount],
                 "Date" : [pickup_dt],
-                "Time" : [pickup_tm],
-                "Distance" : [distance]}
+                "Time" : [pickup_tm]}
     df_ori = pd.DataFrame(ori_data)
     st.write(df_ori)
     dfl = pd.DataFrame({"lat" : [picklat], "long" : [picklong]})
@@ -80,7 +79,7 @@ def run_ml_app():
     # prediction section
     st.subheader("Prediction result")
     st.write("Fare amount :", prediction_reg[0])
-
+    st.write("Distance :", distance)
     st.write("Pickup Location :")
     arr = np.array([[picklat, picklong]])
     df = pd.DataFrame(
