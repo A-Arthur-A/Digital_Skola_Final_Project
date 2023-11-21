325791420c65518b3a2f623fd296c1144a835371
Anthony A. Ataupah <145671960+A-Arthur-A@users.noreply.github.com>
2023-11-21 16:31:58 +0800

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -129,8 +129,6 @@ def run_ml_app():
     scaler = load_scaler("scaler.pkl")
     scaled_data = scaler.transform(ml_data)
 
-    model_reg = load_model("model_lreg.pkl")
-    prediction = model_reg.predict(scaled_data)
     
     
 
